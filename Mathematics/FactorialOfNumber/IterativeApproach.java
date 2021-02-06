42c8d49b6fedad993f7a77378e83853c6691ae6a
anurag2541997 <a.dixit@iiitmanipur.ac.in>
2021-02-06 14:31:43 +0530

    Iterative Approach of Factorial of Number

---
+++ Mathematics/FactorialOfNumber/IterativeApproach.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class IterativeApproach
+{	public static long factorial(long n)
+	{
+		long fact=1;
+		while(n!=0)
+		{
+			fact=fact*n;
+			n--;
+		}
+		return fact;
+	}
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		long n=sc.nextInt();
+		System.out.println("Factorial of "+ n + " is "+factorial(n));
+		
+	}
+}
