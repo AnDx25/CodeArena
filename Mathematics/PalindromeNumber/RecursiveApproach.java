eee705cf247356c418665e191a6178678d89c55d
anurag2541997 <a.dixit@iiitmanipur.ac.in>
2021-02-06 14:20:19 +0530

    RecursiveApproach of Palindrome Number

---
+++ Mathematics/PalindromeNumber/RecursiveApproach.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class RecursiveApproach
+{
+	public static int reverseOfNumber(int rev,int n)
+	{
+		if(n==0)
+		{
+			return rev;
+		}
+		//Taking out last digit and adding to rev*10;
+		rev=rev*10+n%10;
+		return reverseOfNumber(rev,n/10);
+	}
+
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		//Find reverse
+		int rev=0;
+		int n_rev=reverseOfNumber(rev,n);
+		System.out.println(n_rev);
+	}
+}
